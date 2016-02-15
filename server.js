ebcc2d59551746c3726929e154126f3396273ef8
Jam <jamessewart@gmail.com>
2016-02-15 12:33:23 +0000

    change to listen errwhere

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var path = require('path');
 
 // The default port numbers are the standard ones [80,443] for convenience.
 // Change them to e.g. [8080,8443] to avoid privilege or clash problems.
-var ports = [80, 443];
+var ports = [8081, 8443];
 
 // The most common standard file extensions are supported.
 // The most common non-standard file extensions are excluded, with a message.
@@ -45,22 +45,22 @@ var types = {
 function start() {
     test();
     var httpService = http.createServer(serve);
-    httpService.listen(ports[0], 'localhost');
+    httpService.listen(ports[0], '0.0.0.0');
     var options = {
         key: key,
         cert: cert
     };
     var httpsService = https.createServer(options, serve);
-    httpsService.listen(ports[1], 'localhost');
+    httpsService.listen(ports[1], '0.0.0.0');
     printAddresses();
 }
 
 // Print out the server addresses.
 function printAddresses() {
-    var httpAddress = "http://localhost";
+    var httpAddress = "http://0.0.0.0";
     if (ports[0] != 80) httpAddress += ":" + ports[0];
     httpAddress += "/";
-    var httpsAddress = "https://localhost";
+    var httpsAddress = "https://0.0.0.0";
     if (ports[1] != 443) httpsAddress += ":" + ports[1];
     httpsAddress += "/";
     console.log('Server running at', httpAddress, 'and', httpsAddress);
