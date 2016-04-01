d07c052156e8136fa0353352c5060cc1c2842838
Iman <imanapril29@hotmail.com>
2016-04-01 09:42:17 +0300

    fixed resizer

--- main.js
+++ main.js
@@ -386,3 +386,16 @@ function setupSearch() {
         updateSearchResults(results);
     }
 }
+
+
+document.body.setScaledFont = function (f) {
+    var s = this.offsetWidth,
+        fs = s * f;
+    this.style.fontSize = fs + '%';
+    return this
+};
+
+document.body.setScaledFont(0.07);
+window.onresize = function () {
+    document.body.setScaledFont(0.07);
+}
