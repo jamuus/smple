be59caf4231b0b3666fe456dd882adc1a1a1598a
Jam <jamessewart@gmail.com>
2016-05-28 16:04:52 +0100

    test some secure stuff

--- server.js
+++ server.js
@@ -289,66 +289,8 @@ function failTest(s) {
 //     "-----END CERTIFICATE-----\n";
 
 
-// signed by https://letsencrypt.org for thejam.us
-// var cert = "-----BEGIN CERTIFICATE-----\n" +
-//     "MIIE9jCCA96gAwIBAgISAVMEfvi8sousT4+oTDyQifVMMA0GCSqGSIb3DQEBCwUA\n" +
-//     "MEoxCzAJBgNVBAYTAlVTMRYwFAYDVQQKEw1MZXQncyBFbmNyeXB0MSMwIQYDVQQD\n" +
-//     "ExpMZXQncyBFbmNyeXB0IEF1dGhvcml0eSBYMTAeFw0xNjAzMDkxMDE1MDBaFw0x\n" +
-//     "NjA2MDcxMDE1MDBaMBQxEjAQBgNVBAMTCXRoZWphbS51czCCASIwDQYJKoZIhvcN\n" +
-//     "AQEBBQADggEPADCCAQoCggEBAL4VRvYcRF2O8vc3QzXEDS35hX/0tGhPB838V2jJ\n" +
-//     "HTG957OhVc5HFgA4UhGpukCCb3B+hdQNaPHi6kBom3JpjFa9kHhohQnt8fBDHSTX\n" +
-//     "24OceyVVdsJEVl2sDCkHIUkK9E00rN0q5wBmIgrFI7MNuJ1L4x1ekfwuAjA6Dkg+\n" +
-//     "ylPm8wm2A8UWWHGsx8PJKLPEks8HqYlo+L+9GCnRmsSoDq4vZyTV2MErjRMPIHkF\n" +
-//     "F6jcyFm+cFlvVDapIYc1BIBO0Cjtqo2MbU+GQQtfV/z3X2nZEohkfLZ5eHM0YvxU\n" +
-//     "Oish6/RE3Yldwqjxdwe2wFSB00sbC/LtkBbpRyh4KsfgjnMCAwEAAaOCAgowggIG\n" +
-//     "MA4GA1UdDwEB/wQEAwIFoDAdBgNVHSUEFjAUBggrBgEFBQcDAQYIKwYBBQUHAwIw\n" +
-//     "DAYDVR0TAQH/BAIwADAdBgNVHQ4EFgQUM/ts2B5W0KorH77TwqX8nKJYOgowHwYD\n" +
-//     "VR0jBBgwFoAUqEpqYwR93brm0Tm3pkVl7/Oo7KEwcAYIKwYBBQUHAQEEZDBiMC8G\n" +
-//     "CCsGAQUFBzABhiNodHRwOi8vb2NzcC5pbnQteDEubGV0c2VuY3J5cHQub3JnLzAv\n" +
-//     "BggrBgEFBQcwAoYjaHR0cDovL2NlcnQuaW50LXgxLmxldHNlbmNyeXB0Lm9yZy8w\n" +
-//     "FAYDVR0RBA0wC4IJdGhlamFtLnVzMIH+BgNVHSAEgfYwgfMwCAYGZ4EMAQIBMIHm\n" +
-//     "BgsrBgEEAYLfEwEBATCB1jAmBggrBgEFBQcCARYaaHR0cDovL2Nwcy5sZXRzZW5j\n" +
-//     "cnlwdC5vcmcwgasGCCsGAQUFBwICMIGeDIGbVGhpcyBDZXJ0aWZpY2F0ZSBtYXkg\n" +
-//     "b25seSBiZSByZWxpZWQgdXBvbiBieSBSZWx5aW5nIFBhcnRpZXMgYW5kIG9ubHkg\n" +
-//     "aW4gYWNjb3JkYW5jZSB3aXRoIHRoZSBDZXJ0aWZpY2F0ZSBQb2xpY3kgZm91bmQg\n" +
-//     "YXQgaHR0cHM6Ly9sZXRzZW5jcnlwdC5vcmcvcmVwb3NpdG9yeS8wDQYJKoZIhvcN\n" +
-//     "AQELBQADggEBABfZOCQOvcfEhKIZo/ROyfznHDERSMd2DhrhTXsM9pqWAuWlyFa0\n" +
-//     "s/Gcr0DaQF/z8C3qqIKS0yY8J5rgcHEtz3LPqD7+ACWNgnG9hRTyGKX1sJTiXOwC\n" +
-//     "aLX5IheUN4/FRFkfd41QEHdlKfPBAzCGtJlMADj7hHJ+CWSAunWegYUW54kw5Zs5\n" +
-//     "ZRZoavjPb67CTVbBaTd0dhMEXK7yswi39WwJ8s/TyQfcVDATYphUW8sNT/JAOWt4\n" +
-//     "VlvUqzgIPifz9Wl+JXO590BlMsTE4qleWJM0YAK44DobSVKjUFukBIaLlM1GtTkY\n" +
-//     "TyIOcB8x3j0tPi6cxhdiLY/mvOZTjxTyulM=\n" +
-//     "-----END CERTIFICATE-----\n";
 
-// var key = "-----BEGIN PRIVATE KEY-----\n" +
-//     "MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC+FUb2HERdjvL3\n" +
-//     "N0M1xA0t+YV/9LRoTwfN/FdoyR0xveezoVXORxYAOFIRqbpAgm9wfoXUDWjx4upA\n" +
-//     "aJtyaYxWvZB4aIUJ7fHwQx0k19uDnHslVXbCRFZdrAwpByFJCvRNNKzdKucAZiIK\n" +
-//     "xSOzDbidS+MdXpH8LgIwOg5IPspT5vMJtgPFFlhxrMfDySizxJLPB6mJaPi/vRgp\n" +
-//     "0ZrEqA6uL2ck1djBK40TDyB5BReo3MhZvnBZb1Q2qSGHNQSATtAo7aqNjG1PhkEL\n" +
-//     "X1f8919p2RKIZHy2eXhzNGL8VDorIev0RN2JXcKo8XcHtsBUgdNLGwvy7ZAW6Uco\n" +
-//     "eCrH4I5zAgMBAAECggEAUrsYA44SA1ZwUUDwM7p8sgHkJOjwjGW5U+H8eVLvLfMl\n" +
-//     "oX0ax4kQ/k+FCMMCmYkrz56ByOV9Q6oropTk80sVFbuz4XQ8UzIJFzVeveZlWEcH\n" +
-//     "Ihysb6kmneZ/9GtyBUSLR/8hLbG6kOXi8yUSgJ/8NhoNY38BsuyjbzIVfUQ284TU\n" +
-//     "m/mhWAIRwePrROEyQ6+PBqP13dKeB8ZaG1hBUkgaAqBkVq93yLskUwY73TAEXS5E\n" +
-//     "p7ZpY6j4xjCu+XaksITKiMWNl63XPcrvUx24tc3WR4ZzvLTanCwCE2Mg+EY1GqYu\n" +
-//     "8P0J/hh//O8o9pGY0IHFH+0672US8wrU4Fqm+lp8wQKBgQDvqxi6pDH4EN4gtvwT\n" +
-//     "ze45msN6n0yMH6SI9FBPLuXuRcB2wbQnvPU6QyT8ngRx5a3dqrBmCUZo9cnpbkDf\n" +
-//     "FNvViipyVIzKlvEOp/9Ee6I3YG6a4mrA2eomctfZ1QQI+NYg6K1SK1uUayJyHpCS\n" +
-//     "Zps8Oxbwz54fGuBGYvakS/qV6QKBgQDLCTBtHv6jnlL30SG9lQLNwBq29TxN8hZR\n" +
-//     "FmUSe1v9WG+8846yRWpeUQXBv0DSclmDWNUX+OcTQMl9SulSMaxkFELbe6IgewgH\n" +
-//     "k9SSah5fdBpYmk2HlSHOb1D5DtZM+Z3z+m6gxWmyRGv9tx43GchiRmH+YHJ+hIJq\n" +
-//     "if0ZBGAb+wKBgBLmOxz8tbQKIHoT8+zb4F1Khv+0cCTcmezy1yJnYFpZxcOXos0/\n" +
-//     "aVce1FvXWiJhKkTAoQhq0tKUD0gJGbR9wJgmPRKm+DNBk+DD/q030qLrR82O7Twn\n" +
-//     "8v71L3BOC/NpK/mMX56LLL2XdS/qmRvyW2t0fWqf9KgfRnBGfYyXMTuBAoGAHf+O\n" +
-//     "kyDYOK6Eza6tkIg6sNGoYM3dChsxputrJY7qaYUuhTlrJPXSoHrSIe0zE6TnituO\n" +
-//     "KIuTAKo62vM9g/Jo6SSBOFKNAsWKyyvRZYyeTjYmSl8KA3VKWGjkCthhW2AqMUkY\n" +
-//     "HVLtqfQoDIWIxlVd4P9LLT1szTqg1kLrDU4zMQ8CgYBOo0gElS1n8EKZox0CDWFF\n" +
-//     "aGBDEWp0PgmedMA6KURtYD/tksnE0kePoTO00ZszUwUQq7nxS2kdLfduatnrdpe5\n" +
-//     "UWdfegSDap1abe6PJaWncl2Q+99Qm77nJaPx1oxVmeCHwrzg2Gd8adtjYbPUm4Aw\n" +
-//     "bDkEiHm+LvJNCIaEN5nQcQ==\n" +
-//     "-----END PRIVATE KEY-----\n";
-
+// signed by letsencrypt for smple.uk
 var cert = '-----BEGIN CERTIFICATE-----\n' +
     'MIIE9DCCA9ygAwIBAgISAzAOjysm7KpQH9yOFEVs2PT2MA0GCSqGSIb3DQEBCwUA\n' +
     'MEoxCzAJBgNVBAYTAlVTMRYwFAYDVQQKEw1MZXQncyBFbmNyeXB0MSMwIQYDVQQD\n' +
