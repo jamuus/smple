4c848f92260bfb87f06396443c160693e7fe46fd
Jam <jamessewart@gmail.com>
2016-05-29 18:08:20 +0100

    ^

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var svgNS = "http://www.w3.org/2000/svg";
 var svgButton;
 var startDate = new Date();
 var endDate = new Date();
-endDate.setDate(endDate.setDate() + 7);
+endDate.setDate(endDate.getDate() + 7);
 
 window.onload = function() {
     svg = document.getElementById("samplebutton");
